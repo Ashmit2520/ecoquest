ddb0fc382d99607d17262524218c0cf62ba2f966
Ash2520 <ashmitspl@gmail.com>
2026-02-28 18:20:20 -0600

    test

--- src/components/CompletionModal.tsx
+++ src/components/CompletionModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { Quest } from '@/data/quests';
+import { Quest } from '../data/quests';
 
 interface CompletionModalProps {
   quest: Quest;
